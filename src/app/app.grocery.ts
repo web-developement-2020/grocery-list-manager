cb524110d21e73df7429ca8980b72651e6dab0de
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-09-01 22:34:39 -0700

    new changes

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -4,15 +4,54 @@ import { Component } from '@angular/core';
   selector: 'app-grocery',
   templateUrl: './app.grocery.html',
   styleUrls: [
-
-
+    '../assets/css/bootstrap-3.3.7-dist/css/bootstrap.min.css'
   ]
 })
+
 export class GroceryComponent {
-  task: string;
+  task = {
+    name: '',
+    id: 0
+  };
   tasks = [];
+
   onClick() {
-    this.tasks.push({ name: this.task });
-    this.task = '';
+    if (this.task.id == 0) {
+      this.tasks.push({ id: (new Date()).getTime(), name: this.task.name, strike: false });
+    }
+
+    this.task = {
+      name: '',
+      id: 0
+    };
   }
+
+  onEdit(item) {
+    this.task = item;
+  }
+
+  onDelete(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        this.tasks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  onStrike(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        if (this.tasks[i].strike) {
+          this.tasks[i].strike = false;
+        }
+        else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
+
+
 }
